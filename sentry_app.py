e2e0bf213878cf594fb1372642e16e35a1f4d6a3
Fuiradan <56451454+Fuiradan@users.noreply.github.com>
2020-06-05 05:59:11 +0500

    Add files via upload

--- sentry_app.py
+++ sentry_app.py
@@ -2,6 +2,8 @@ import sentry_sdk
 import math
 sentry_sdk.init("https://6108b3617c624ea391c9f3a13401fd18@o400108.ingest.sentry.io/5258203")
 
+with sentry_sdk.configure_scope() as scope:
+    scope.set_tag('sentry_tag', 'here')
 
 a = -1
 
@@ -11,15 +13,5 @@ try:
 except Exception:
     sentry_sdk.capture_message('Ошибка №54565616. Входная переменная отрицательная')
 
-
 #ошибка деления на ноль
 division_by_zero = 1 / 0
-
-with sentry_sdk.configure_scope() as scope:
-    scope.set_tag('sentry_tag')
-    scope.set_tag('Lab_6')
-
-mas = [1,1,1,1,1,1,1]
-sum = 0
-for i in range(mas):
-   sum += 1 
